9bb97ce27b9968de9f1134bacc7415e9995b67da
hnry-dngr <nwokobiah@gmail.com>
2024-01-25 20:54:26 +0100

    Create Support vector machines.py

---
+++ Support vector machines.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 25 20:52:08 2024
+
+@author: HP
+"""
+
+# Instructions:
+
+# Import you data and perform basic data exploration phase
+# Display general information about the dataset
+# Create a pandas profiling reports to gain insights into the dataset
+# Handle Missing and corrupted values
+# Remove duplicates, if they exist
+# Handle outliers, if they exist
+# Encode categorical features
+# Select your target variable and the features
+# Split your dataset to training and test sets
+# Build and train an SVM model on the training set
+# Assess your model performance on the test set using relevant evaluation metrics
+# Discuss with your cohort alternative ways to improve your model performance
